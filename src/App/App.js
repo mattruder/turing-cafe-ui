2598caead8334f3019a8a44316a1a4d713d12345
Matt Ruder <matt.j.ruder@gmail.com>
2022-05-25 10:46:51 -0600

    Add form submittal functionality

--- src/App/App.js
+++ src/App/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import ReservationsAll from '../ReservationsAll.js'
 import Reservation from '../Reservation.js'
+import ReservationForm from '../ReservationForm.js'
 
 class App extends Component {
 
@@ -10,6 +11,8 @@ class App extends Component {
     this.state = {
       allReservations: []
     }
+
+    this.updateDom = this.updateDom.bind(this)
   }
 
   componentDidMount = () => {
@@ -18,7 +21,11 @@ class App extends Component {
     .then(data => this.setState({ allReservations: data }))
   }
 
-
+  updateDom() {
+    fetch('http://localhost:3001/api/v1/reservations')
+    .then(response => response.json())
+    .then(data => this.setState({ allReservations: data }))
+  }
 
 
   render() {
@@ -26,7 +33,7 @@ class App extends Component {
       <div className="App">
         <h1 className='app-title'>Turing Cafe Reservations</h1>
         <div className='resy-form'>
-
+          <ReservationForm updateDom={this.updateDom}/>
         </div>
         <div className='resy-container'>
           {this.state.allReservations && <ReservationsAll allReservations={this.state.allReservations} />}
---
+++ src/ReservationForm.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react'
+import './ReservationForm.css'
+
+class ReservationForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      date: '',
+      time: '',
+      guests: ''
+    }
+
+  this.handleChange = this.handleChange.bind(this);
+  this.handleSubmit = this.handleSubmit.bind(this);
+
+  }
+
+  handleChange(event) {
+     this.setState({[event.target.name]: event.target.value});
+   }
+
+   handleSubmit(event) {
+     this.setState({
+       name: '',
+       date: '',
+       time: '',
+       guests: ''
+     })
+     event.preventDefault();
+   }
+
+   makeReservation(name, date, time, guests) {
+     const newDate = date.slice(5).replaceAll("-", "/")
+     const reservation = {
+       id: Date.now(), name: name, date: newDate, time: time, number: guests
+     }
+     fetch('http://localhost:3001/api/v1/reservations', {
+       method: 'POST',
+       body: JSON.stringify(reservation),
+       headers: {
+         'Content-Type' : 'application/json'
+       }
+     })
+     .then(response => response.json())
+     .then(this.props.updateDom)
+   }
+
+   render() {
+   return (
+     <form className="reservation-form" onSubmit={this.handleSubmit}>
+       <label>
+         Name:
+         <input name="name" type="text" value={this.state.name} onChange={this.handleChange} />
+       </label>
+       <label>
+         Date:
+         <input name="date" type="date" value={this.state.date} onChange={this.handleChange} />
+       </label>
+       <label>
+        Time:
+         <input name="time" type="time" value={this.state.time} onChange={this.handleChange} />
+       </label>
+       <label>
+         Guests:
+         <input name="guests" type="number" value={this.state.guests} onChange={this.handleChange} />
+       </label>
+       <input type="submit" value="Make Reservation" onClick={() => {this.makeReservation(this.state.name, this.state.date, this.state.time, this.state.guests)}}/>
+     </form>
+   );
+ }
+
+
+}
+
+export default ReservationForm
